38aa232e844479ed1c9d8f90498e3d0d1354ca2e
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-04-06 00:18:22 +0900

    update: Markuplintのアプデ 4.9.2 -> 4.14.1 + 設定更新(#14)

--- .markuplintrc.mjs
+++ .markuplintrc.mjs
@@ -1,5 +1,5 @@
-/** @type {import("markuplint").Config} */
+/** @type {import("@markuplint/ml-config").Config} */
 export default {
-  extends: ["markuplint:recommended-static-html"],
-  excludeFiles: ["./node_modules/**/*.html"],
+  extends: ['markuplint:recommended-static-html'],
+  excludeFiles: ['./node_modules/**', './dist/**'],
 };
